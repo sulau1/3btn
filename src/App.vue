9d953b86cd2618feec351cb1793f5b7c8462b8a8
sulau1 <99355115+sulau1@users.noreply.github.com>
2023-01-27 23:06:34 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
---
+++ src/popup.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="popup">
+    <div class="popup_head">
+      <span>PopupName</span>
+      <span class="close_button">&#10008;</span>
+    </div>
+    <div class="popup_content">
+      <slot></slot>
+    </div>
+    <div class="popup_bottom">
+      <button></button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "popup",
+  props: {},
+  data() {
+    return {
+      IsPopUpVisible: false,
+    };
+  },
+  methods: {
+    ShowPopup() {
+      this.IsPopUpVisible = true;
+    },
+  },
+};
+</script>
+
+<style></style>
